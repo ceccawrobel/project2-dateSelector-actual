e09c4ce6fa540e918956dea6a1d3853c915c12a6
f wrobel <ceccawrobel@gmail.com>
2019-06-16 15:02:23 -0700

    udpates js file with comments regarding my understanding of why certian parts of the logic are failing

--- js/scripts.js
+++ js/scripts.js
@@ -35,14 +35,53 @@ $(document).ready(function() {
     console.log("age min is " + seekAgeMax);
     console.log("gender is " + seekGender);
 
-    if (seekAgeMin < 33 && seekAgeMax > 33) {
-      $("#maleContestants33").prepend("<b>");
-      // $("#maleContestants33").append("</b>");
+    if (seekAgeMin <= 30 && seekAgeMax >= 30) {
+      $("#maleContestants30").prepend("<b>");
+      $("#maleContestants30").append("</b>");
+      $("#femaleContestants30").prepend("<b>");
+      $("#femaleContestants30").append("</b>");
+      $("#nonbinaryContestants30").prepend("<b>");
+      $("#nonbinaryContestants30").append("</b>");
       // why is this not actually appending, coming at the end of the div? before/after tags don't seem to work either -- they *both* show up when I insert only <b> (as a string!), and neither shows up when I only request to insert </b> ... why can't I control this?
+    } else if (seekAgeMin <= 31 && seekAgeMax >= 31) {
+      $("#maleContestants31").prepend("<b>");
+      $("#maleContestants31").append("</b>");
+      $("#femaleContestants31").prepend("<b>");
+      $("#femaleContestants31").append("</b>");
+      $("#nonbinaryContestants31").prepend("<b>");
+      $("#nonbinaryContestants31").append("</b>");
+      // realizing that this strategy doesn't work (regardless of inefficiency) because after "if" statement, "else if" statements don't run at all, so even if the first one worked to highlight the 30-year-old divs, the others wouldn't run. need to rethink this.
+    } else if (seekAgeMin <= 32 && seekAgeMax >= 32) {
+      $("#maleContestants32").prepend("<b>");
+      $("#maleContestants32").append("</b>");
+      $("#femaleContestants32").prepend("<b>");
+      $("#femaleContestants32").append("</b>");
+      $("#nonbinaryContestants32").prepend("<b>");
+      $("#nonbinaryContestants32").append("</b>");
+    } else if (seekAgeMin <= 33 && seekAgeMax >= 33) {
+      $("#maleContestants33").prepend("<b>");
+      $("#maleContestants33").append("</b>");
+      $("#femaleContestants33").prepend("<b>");
+      $("#femaleContestants33").append("</b>");
+      $("#nonbinaryContestants33").prepend("<b>");
+      $("#nonbinaryContestants33").append("</b>");
+    } else if (seekAgeMin <= 34 && seekAgeMax >= 34) {
+      $("#maleContestants34").prepend("<b>");
+      $("#maleContestants34").append("</b>");
+      $("#femaleContestants34").prepend("<b>");
+      $("#femaleContestants34").append("</b>");
+      $("#nonbinaryContestants34").prepend("<b>");
+      $("#nonbinaryContestants34").append("</b>");
+    } else if (seekAgeMin <= 35 && seekAgeMax >= 35) {
+      $("#maleContestants35").prepend("<b>");
+      $("#maleContestants35").append("</b>");
+      $("#femaleContestants35").prepend("<b>");
+      $("#femaleContestants35").append("</b>");
+      $("#nonbinaryContestants35").prepend("<b>");
+      $("#nonbinaryContestants35").append("</b>");
+    } else {
+      alert("Sorry, we can only point out matches between 30 and 35.")
     }
-    // else if (xx) {
-    //   xx;
-    // }
     event.preventDefault();
   });
 
